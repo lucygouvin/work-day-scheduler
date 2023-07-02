77c488e48a17216559f62d6addcee8c8af24f5fd
lgouvin <gouvin.lucy@gmail.com>
2023-07-01 23:02:42 -0400

    added local storage functionality, comments, readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,95 +1,80 @@
-currentDayEl = $("#currentDay")
-var now;
-var hourActivities = [
-  {0:""},
-  {1:""},
-  {2:""},
-  {3:""},
-  {4:""},
-  {5:""},
-  {6:""},
-  {7:""},
-  {8:""},
-  {9:""},
-  {10:""},
-  {11:""},
-  {12:""},
-  {13:""},
-  {14:""},
-  {15:""},
-  {16:""},
-  {17:""},
-  {18:""},
-  {19:""},
-  {20:""},
-  {21:""},
-  {22:""},
-  {23:""},
-]
+// Following do not interact with the DOM, are safe to be called outisde of the $(function(){})
+// Use DayJS to get the current day
+var today = dayjs();
+// Number of miliseconds until the next hour
+var rerun = (60 - parseInt(today.format("m"))) * 60000
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// See if there is already data saved to local storage. If not, create an empty array.
+var calendar = JSON.parse(localStorage.getItem("calendar"))
+if (calendar === null) {
+  // Created an array of length 24 in case we ever wanted to change the displayed hours in the future
+  calendar = Array(24).fill("")
+}
+
+// Wrap all calls to functions that deal with the DOM in this function, so that it doesn't run until all elements are loaded
 $(function () {
-  // TODO: Add code to display the current date in the header of the page.
-  now = dayjs("2023-06-29 13:00:00");
-  currentDayEl.text(now.format("MMMM D, YYYY"))
+  // Format the day and display it on the HTML element
+  $("#currentDay").text(today.format("[Today is ]MMMM D, YYYY"))
 
+  // Load any locally saved entries
+  loadEntry()
 
-    // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  // Run checkTime function to apply colors to the hour blocks
   checkTime()
 
+  // Rerun the checkTime function at the top of the next hour 
+  setTimeout(function () {
+    checkTime()
+    // Now that we're at the top of the hour, set a timer interval to run checkTime every 60 minutes, so that the time blocks will continue to update
+    setInterval(function () {
+      checkTime()
+    }, 3600000)
+  }, rerun)
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-$(".container-lg").on("click", ".saveBtn", saveEntry)
+  // Use event delegation to listen for clicks on the saveBtn inside the containter-lg
+  $(".container-lg").on("click", ".saveBtn", saveEntry)
+})
 
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
 
-  
+// FUNCTIONS DECLARED BELOW
+function loadEntry() {
+  // Go through the calendar object, construct the time-block's id from the index of the array
+  for (var i = 0; i < calendar.length; i++) {
+    // If no element exists with that id, nothing will happen
+    var index = "#hour-" + i
+    // Get that element's child with the class of description, and set the value equal to the activity stored in the array
+    $(index).children(".description").val(calendar[i])
+  }
+}
 
-});
-
-function checkTime(){
+function checkTime() {
+  // Because this runs every hour, it needs to update what time it is "now" every time it runs
+  var now = dayjs()
+  // Get just the hour, in 24-hour format, from the current day
   var currentHour = parseInt(now.format("H"))
+  // Get an array of all the time-block elements on the page
   var timeBlocks = $(".time-block").get()
-
-
-  for (var i = 0; i<timeBlocks.length; i++){
-    var hour = parseInt(($(timeBlocks[i]).attr("id")).replace("hour-",""))
-
-    if (hour<currentHour){
+  // for each time-block element, get its id, and use that to detemrine the hour it represents
+  for (var i = 0; i < timeBlocks.length; i++) {
+    var hour = parseInt(($(timeBlocks[i]).attr("id")).replace("hour-", ""))
+    // Compare the hour from the element's id to the current hour, and use that to determine if it is past, present, or future
+    if (hour < currentHour) {
       $(timeBlocks[i]).addClass("past")
-    }else if (hour === currentHour){
+    } else if (hour === currentHour) {
       $(timeBlocks[i]).addClass("present")
-    }else{
+    } else {
       $(timeBlocks[i]).addClass("future")
     }
-
-
   }
-  
-
 }
 
-function saveEntry(event){
-  var parentEl = $(event.target).parent()
-  localStorage.setItem(parentEl.attr("id"),parentEl.children(".description").val())
-}
-
-function loadEntry(){
-
-
+function saveEntry(event) {
+  // Get the closest ancestor with the time-block class
+  var parentEl = $(event.target).closest(".time-block")
+  // Modify the time-block element's id to derive the index at which the activity should be stored in the calendar array
+  var index = parseInt(parentEl.attr("id").replace("hour-", ""))
+  var description = parentEl.children(".description").val().trim()
+  calendar[index] = description
+  // Save to local storage
+  localStorage.setItem("calendar", JSON.stringify(calendar))
 }
